f1df2ce92d52adb34c9c5d0ef9daf63e440668e8
u067265 <ely.de.la.cruz@bloomingdales.com>
2016-12-12 10:26:25 -0500

    Started 'Extendable'.

--- src/functor/Extendable.js
+++ src/functor/Extendable.js
@@ -1,3 +1,20 @@
 /**
  * Created by edlc on 12/9/16.
  */
+'use strict';
+import Functor from './Functor';
+import {defineSubClass} from './../defineSubClass';
+
+let Extendable = defineSubClass (Functor,
+    function Extendable (value) {
+        if (!this) {
+            return new Extendable(value);
+        }
+        Functor.call(this, value);
+    }, {
+        extend: function (fn) {
+            return this.map(fn);
+        }
+    });
+
+export default Extendable;
