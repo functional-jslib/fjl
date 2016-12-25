a7b276d908db515ceda11d6618628d81f0190b07
Ely De La Cruz <elycruz@elycruz.com>
2016-12-25 14:29:43 -0500

    Removed some comments.

--- src/monad/Either.js
+++ src/monad/Either.js
@@ -1,10 +1,6 @@
 /**
  * Created by elyde on 12/10/2016.
  */
-'use strict';
-/**
- * Created by elyde on 11/20/2016.
- */
 
 'use strict';
 
--- tests/helpers.js
+++ tests/helpers.js
@@ -6,7 +6,17 @@
 import {curry2} from './../src/curry';
 import {expect} from 'chai';
 
-export let add = curry2((...args) => {
+export let  expectInstanceOf = curry2((value, instance) => expect(value).to.be.instanceOf(instance)),
+
+    expectFunction = value => expect(value).to.be.instanceOf(Function),
+
+    expectEqual = curry2((value, value2) => expect(value).to.be.equal(value2)),
+
+    expectFalse = value => expectEqual(value, false),
+
+    expectTrue = value => expectEqual(value, true),
+
+    add = curry2((...args) => {
         return args.reduce((agg, num) => num + agg, 0);
     }),
 
@@ -16,25 +26,15 @@ export let add = curry2((...args) => {
 
     divide = curry2((...args) => {
         return args.reduce((agg, num) => agg / num, args.shift());
-    }),
-
-    expectInstanceOf = curry2((value, instance) => expect(value).to.be.instanceOf(instance)),
-
-    expectFunction = value => expect(value).to.be.instanceOf(Function),
-
-    expectEqual = curry2((value, value2) => expect(value).to.be.equal(value2)),
-
-    expectFalse = value => expectEqual(value, false),
-
-    expectTrue = value => expectEqual(value, true);
+    });
 
 export default {
-    add,
-    multiply,
-    divide,
     expectFunction,
     expectInstanceOf,
     expectEqual,
     expectFalse,
-    expectTrue
+    expectTrue,
+    add,
+    multiply,
+    divide
 };
