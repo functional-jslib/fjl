37e52838beff1e3881dc8be4832398ad4b4b832d
u067265 <ely.de.la.cruz@bloomingdales.com>
2017-05-01 17:50:14 -0400

    operators,fnOperators - Added 'call' and 'apply' initial functions.

---
+++ src/fnOperators.js
@@ -0,0 +1,11 @@
+/**
+ * Created by u067265 on 5/1/17.
+ */
+
+const call = (fn, x, ...args) => fn.call(x, ...args),
+    apply = (fn, x, ...args) => fn.apply(x, args);
+
+export default {
+    call,
+    apply
+};
