7648ac411deddbea54c6a43cf4483f45139b9225
Ely De La Cruz <elycruz@elycruz.com>
2017-09-10 03:28:48 -0400

    jsPlatform - Progress .

--- src/jsPlatform/listOpsUnCurried.js
+++ src/jsPlatform/listOpsUnCurried.js
@@ -1,7 +1,11 @@
+/**
+ *  List operations that overlap (apart from globally overlapping props and functions like `length` and `toString`)
+ *      on both strings and arrays.
+ */
 
 import {fPureTakesOne, fPureTakes2, fPureTakesOneOrMore} from '../utils/utils';
 
-export {length, toString} from '../objectOps/objectPrelude';
+// export {length, toString} from './objectOpsUnCurried';
 
 export const
 
@@ -9,7 +13,9 @@ export const
 
     slice = fPureTakes2('slice'),
 
-    includes = fPureTakesOne('includes'), // @todo shim here if not supported necessary
+    includes = (() => 'includes' in Array.prototype ?
+            fPureTakesOne('includes') :
+            (value, x) => x.indexOf(value) > -1)(),
 
     indexOf = fPureTakesOne('indexOf'),
 
---
+++ src/jsPlatform/objectOpsUnCurried.js
@@ -0,0 +1,105 @@
+/**
+ * Created by elydelacruz on 9/6/2017.
+ * Defines some of the platform methods for objects (the ones used within `fjl`) uncurried for use
+ * throughout the library.  @note Doesn't include all methods for objects just the ones used in
+ *  the library.
+ * @todo change all files named '*UnCurried' to '*Uncurried'.
+ */
+
+import {fPureTakesOne} from '../utils/utils';
+
+export const
+
+    /**
+     * Returns a boolean indicating whether a "value" is a pure object or not.
+     * @function module:jsPlatform.objectOpsUncurried.isObject
+     * @param x {*}
+     * @returns {Boolean}
+     */
+    isObject = x => typeOf(x) === 'Object',
+
+    /**
+     * Returns whether constructor has derived objectOps.
+     * @function module:jsPlatform.objectOpsUncurried.instanceOf
+     * @param instanceConstructor {Function} - Constructor.
+     * @param instance {*}
+     * @instance {*}
+     * @returns {Boolean}
+     */
+    instanceOf = (instanceConstructor, instance) =>
+        instance instanceof instanceConstructor,
+
+    /**
+     * @function module:jsPlatform.objectOpsUncurried.hasOwnProperty
+     * @param propName {*}
+     * @param typeInstance {*}
+     * @returns {Boolean}
+     */
+    hasOwnProperty = fPureTakesOne('hasOwnProperty'),
+
+    /**
+     * @function module:jsPlatform.objectOpsUncurried.length
+     * @param x {*}
+     * @returns {Number}
+     * @throws {Error} - Throws an error if value doesn't have a `length` property (
+     *  `null`, `undefined`, {Boolean}, Symbol, et. al.).
+     */
+    length = x => x.length,
+
+    /**
+     * @function module:jsPlatform.objectOpsUncurried.hasOwnProperty
+     * @param x {*}
+     * @returns {Number}
+     * @throws {Error} - Throws an error if value doesn't have a `toString`.
+     */
+    toString = x => x.toString(),
+
+    /**
+     * Gets own enumerable keys of passed in object (`Object.keys`).
+     * @function module:jsPlatform.objectOpsUncurried.keys
+     * @param obj {*}
+     * @returns {Array<String>}
+     */
+    keys = obj => Object.keys(obj),
+
+    /**
+     * Defined as `Object.assign` else is the same thing but shimmed.
+     * @function module:jsPlatform.objectOpsUncurried.assign
+     * @param obj0 {Object}
+     * @param objs {...{Object}}
+     * @returns {Object}
+     */
+    assign = (() =>
+        Object.assign ?
+            (obj0, ...objs) => Object.assign(obj0, ...objs) :
+            (obj0, ...objs) => objs.reduce((topAgg, obj) => {
+                return keys(obj).reduce((agg, key) => {
+                    agg[key] = obj[key];
+                    return agg;
+                }, topAgg);
+            }, obj0))(),
+
+    /**
+     * Merges all objects down into one.
+     * @function module:jsPlatform.objectOpsUncurried.assignDeep
+     * @param obj0 {Object}
+     * @param objs {...{Object}}
+     * @returns {Object}
+     */
+    assignDeep = (obj0, ...objs) =>
+        objs.reduce((topAgg, obj) =>
+            keys(obj).reduce((agg, key) => {
+                let propDescription = Object.getOwnPropertyDescriptor(agg, key);
+                // If property is not writable move to next item in collection
+                if (hasOwnProperty(key, agg) && propDescription &&
+                    !(propDescription.get && propDescription.set) &&
+                    !propDescription.writable) {
+                    return agg;
+                }
+                if (isObject(agg[key]) && isObject(obj[key])) {
+                    assignDeep(agg[key], obj[key]);
+                }
+                else { agg[key] = obj[key]; }
+                return agg;
+            }, topAgg)
+        , obj0);
