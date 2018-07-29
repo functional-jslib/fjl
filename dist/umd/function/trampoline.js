b56eee638fc199b0b5959e96d71476ef8b82eb75
Ely De La Cruz <elycruz@elycruz.com>
2018-07-29 12:51:36 -0400

    next - Added 'next' to version.

--- dist/umd/function/trampoline.js
+++ dist/umd/function/trampoline.js
@@ -19,7 +19,23 @@
     /**
      * Trampolines function calls in order to avoid stack overflow errors
      * on recursive function calls; Tail recursion replacement.
-     * @see https://jsperf.com/pure-trampoline/1
+     * @example
+     * // Instead of ... (which is prone to stack-overflow in
+     * //   non-tail-call optimized environments (es5-es3))
+     * const factorial = n => n ? n * factorial(n - 1) : 1;
+     *
+     * // We do
+     * const
+     *
+     *  factorialProcess = (n, agg = 1) => {
+     *      n ? () => factorialProcess(n - 1, agg * n) : agg,
+     *  },
+     *
+     *  factorial = n => trampoline(factorialProcess)(n);
+     *
+     *  ;
+     *
+     * @performance https://jsperf.com/pure-trampoline/1
      * @function module:function.trampoline
      * @param fn {Function} - Function to trampoline.
      * @param [fnName=undefined] {String} - Optionally restrict trampolining only to function with specific name.
