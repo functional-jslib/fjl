6a78d95e8a297082fe677b25d8b989478f6aa7a8
Ely De La Cruz <elycruz@elycruz.com>
2019-01-21 11:51:11 -0500

    object-typescript types progress.

--- types/object.d.ts
+++ types/object.d.ts
@@ -1,3 +1,4 @@
+import {Runtime} from "inspector";
 declare namespace fjl {
 
     export function instanceOf(Constructor: Function, obj: any): boolean;
@@ -8,4 +9,31 @@ declare namespace fjl {
 
     export function assign(obj0: any, ...objs: any[]): string[];
 
+    export namespace native {
+        export function defineProperties<T>(properties: PropertyDescriptorMap, target: T): T
+
+        export function defineProperty<T>(propertyDescriptor: PropertyDescriptor, propertyName: string, target: T): T
+
+        export function assign(obj0: any, ...objs: Array<any>): any;
+
+        // @todo add the rest of the methods here
+    }
+
+    export function lookup(key: string, obj: any): any;
+
+    export function assign(obj0: any, ...objs: Array<any>): any;
+
+    export function typeOf(x: any): string;
+
+    export function copy(x: any): any;
+
+    export function toTypeRef(x: string | Function | any): string | Function;
+
+    export function toTypeRefName(x: string | Function | any): string;
+
+    export function toTypeRefNames(...types: Array<string | Function | any>): Array<string>;
+
+    export function isFunction(x: any): boolean;
+
+    export function isType(type: string | Function, x: any): boolean;
 }
