383a85985c0b96a66f0d400de4ef7e47e651bf14
Ely De La Cruz <elycruz@elycruz.com>
2019-07-06 21:05:18 -0400

    added tests for 'fnOrError'.

---
+++ tests/function/test-fnOrError.ts
@@ -0,0 +1,20 @@
+import {fnOrError} from '../../src/function/fnOrError';
+import {falsyList} from '../helpers';
+
+describe('#fnOrError', () => {
+    it('should be a function', () => {
+        expect(fnOrError).toBeInstanceOf(Function);
+    });
+    it('should have an arity of 2', () => {
+        expect(fnOrError.length).toEqual(2);
+    });
+    it('should throw an error when not receiving a function', () => {
+        falsyList.forEach(f => {
+            expect(() => fnOrError('f', f)).toThrow();
+        });
+    });
+    it ('should not throw an error when receiving a function', () => {
+        const result = fnOrError('fnOrError', fnOrError);
+        expect(result).toBeInstanceOf(Function);
+    });
+});
