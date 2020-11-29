4f55100f0804f258957ec7451883ad890c5812ba
elycruz <603428+elycruz@users.noreply.github.com>
2020-11-28 20:13:38 -0500

    removed test on builds from tests.

--- tests/build-test-cjsLibraryImport.ts
+++
@@ -1,8 +0,0 @@
-const fjl = require('../dist/cjs/fjl'),
-    {expectTrue}  = require('./helpers');
-
-describe ('fjl', function () {
-    it ('should have reached this point with no errors', () => {
-        expectTrue((!!fjl && !!Object.keys(fjl).length)); // Bangs are for compilers that get the `&&` statement wrong
-    });
-});
