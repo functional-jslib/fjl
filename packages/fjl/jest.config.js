f10c6e93d058ed8c7bedaeb710d998ccc62c3593
elycruz <603428+elycruz@users.noreply.github.com>
2020-12-02 21:59:11 -0500

    Progress on rollup setup.

--- packages/fjl/jest.config.js
+++ packages/fjl/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     preset: 'ts-jest',
     testMatch: [
-        '**/packages/**/*@(.test|_test|.spec).ts',
-        '**/packages/**/tests/**/test-*.ts',
+        'src/**/*@(.test|_test|.spec).ts',
+        'tests/**/test-*.ts',
     ],
     testEnvironment: 'node',
     globals: {
