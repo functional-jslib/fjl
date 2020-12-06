b9973097e7815171e6fff5f15f33cb636c97342e
elycruz <603428+elycruz@users.noreply.github.com>
2020-12-05 22:45:30 -0500

    fjl/tests - Updated typing errors to be valid on some list tests.

--- packages/fjl/tests/list/test-slice.ts
+++ packages/fjl/tests/list/test-slice.ts
@@ -1,12 +1,5 @@
-import {slice} from "../../src/platform/slice";
-import {SliceOf} from "../../src/platform/slice";
-import {vowelsString} from "../helpers";
-
 describe('#slice', () => {
-    // (<[number, number, SliceOf<any>][]>[
-    //     [[vowelsString]]
-    // ])
-    //     .forEach(args => {
-    //         expect(slice(...args)).toEqual(args[2].slice(args[0], args[1]));
-    //     })
+  it(`should have more tests `, () => {
+    expect(true).toEqual(false);
+  });
 });
--- packages/fjl/tests/list/test-take.ts
+++ packages/fjl/tests/list/test-take.ts
@@ -1,34 +1,35 @@
 import {expectEqual, expectError, vowelsArray, vowelsString} from "../helpers";
 import {take} from "../../src/list/take";
+import {SliceOf} from "../../src/platform";
 
 describe('#take', () => {
-    it('should return taken items from list and/or string until limit', () => {
-        type TakeTest = [[number, string | any[]], string | any[]];
-        type TakeTestCases = Array<TakeTest>;
-        (<TakeTestCases>[
-            [[0, ''], ''],
-            [[0, []], []],
-            [[1, ''], ''],
-            [[1, []], []],
-        ]).concat(
-            (<TakeTestCases>vowelsArray
-                .map((_, ind) => [
-                    [ind, vowelsArray],
-                    vowelsArray.slice(0, ind)
-                ])),
-            (<TakeTestCases>vowelsString.split('')
-                .map((_, ind) => [
-                    [ind, vowelsString],
-                    vowelsString.slice(0, ind)
-                ]))
-        )
-            .forEach(([args, expected]) => {
-                expectEqual(take(...args), expected);
-            });
-    });
-    it('should throw an error when no parameter is passed in', () => {
-        [null, undefined, 0, {}].forEach(x =>
-            expectError(() => take(3, x))
-        );
-    });
+  it('should return taken items from list and/or string until limit', () => {
+    type TakeTest = [[number, string | any[]], string | any[]];
+    type TakeTestCases = Array<TakeTest>;
+    (<TakeTestCases>[
+      [[0, ''], ''],
+      [[0, []], []],
+      [[1, ''], ''],
+      [[1, []], []],
+    ]).concat(
+      (<TakeTestCases>vowelsArray
+        .map((_, ind) => [
+          [ind, vowelsArray],
+          vowelsArray.slice(0, ind)
+        ])),
+      (<TakeTestCases>vowelsString.split('')
+        .map((_, ind) => [
+          [ind, vowelsString],
+          vowelsString.slice(0, ind)
+        ]))
+    )
+      .forEach(([args, expected]) => {
+        expectEqual(take(...args), expected);
+      });
+  });
+  it('should throw an error when no parameter is passed in', () => {
+    [null, undefined, 0, {}].forEach(x =>
+      expectError(() => take(3, x as SliceOf<any>))
+    );
+  });
 });
