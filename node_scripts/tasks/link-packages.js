84d0d9b446a7d73b3ab3321980a56f6583bc8d53
elycruz <603428+elycruz@users.noreply.github.com>
2021-06-13 14:44:28 -0400

    Moved build step of monorepo workflow to the end of script - running build without sub-packages being linked causes typescript error.

--- node_scripts/tasks/link-packages.js
+++ node_scripts/tasks/link-packages.js
@@ -16,39 +16,40 @@ const
  * @return {Promise<void>}
  */
 (async () => Promise.resolve()
-  .then(() => !process.env.NODE_ENV ? ioSpawn(npmCmd, ['i'], {cwd: repoRoot}) : void 0)
-  .then(() => log('Building packages\n'))
-  .then(() => ioSpawn(npmCmd, ['run', 'build'], {cwd: repoRoot}))
-  .then(() => log('Linking packages\n'))
-  .then(() => [
-      ['fjl'],
-      ['fjl-validator', 'fjl'],
-      // 'fjl-filter',
-      ['fjl-inputfilter', 'fjl', 'fjl-validator'],
-    ]
-      .reduce((p, [packageName, ...packagesToLink]) => {
-          const cwd = path.join(repoRoot, `packages/${packageName}`);
-          // return
-          // .then(() => {
-          //   if (packagesToLink.length) {
-          //     log(`Linking subpackages: ${packagesToLink.join(', ')}`);
-          //     return Promise.all(
-          //       packagesToLink.map(subPackageName =>
-          //         ioSpawn(npmCmd, ['link', subPackageName], {cwd})
-          //       ));
-          //   }
-          // })
-          return p.then(() => {
-            log(`${taskGroupSeparator}Linking ${packageName} ...`);
-            return ioSpawn(npmCmd, ['link'], {cwd});
-          })
-            .then(() => {
-              log(`${taskGroupSeparator}Linking ${packageName} to main project ...`);
-              return ioSpawn(npmCmd, ['link', packageName], {cwd: repoRoot});
+    .then(() => !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? ioSpawn(npmCmd, ['i'], {cwd: repoRoot}) : void 0)
+    .then(() => log('Linking packages\n'))
+    .then(() => [
+        ['fjl'],
+        ['fjl-validator', 'fjl'],
+        // 'fjl-filter',
+        ['fjl-inputfilter', 'fjl', 'fjl-validator'],
+      ]
+        .reduce((p, [packageName, ...packagesToLink]) => {
+            const cwd = path.join(repoRoot, `packages/${packageName}`);
+            // return
+            // .then(() => {
+            //   if (packagesToLink.length) {
+            //     log(`Linking subpackages: ${packagesToLink.join(', ')}`);
+            //     return Promise.all(
+            //       packagesToLink.map(subPackageName =>
+            //         ioSpawn(npmCmd, ['link', subPackageName], {cwd})
+            //       ));
+            //   }
+            // })
+            return p.then(() => {
+              log(`${taskGroupSeparator}Linking ${packageName} ...`);
+              return ioSpawn(npmCmd, ['link'], {cwd});
             })
-            .catch(error);
-        },
-        Promise.resolve()
-      )
-      .then(() => log(taskGroupSeparator + 'Script completed successfully.\n'))
-  ))();
+              .then(() => {
+                log(`${taskGroupSeparator}Linking ${packageName} to main project ...`);
+                return ioSpawn(npmCmd, ['link', packageName], {cwd: repoRoot});
+              })
+              .catch(error);
+          },
+          Promise.resolve()
+        )
+        .then(() => log(taskGroupSeparator + 'Script completed successfully.\n'))
+    )
+    .then(() => log('Building packages\n'))
+    .then(() => ioSpawn(npmCmd, ['run', 'build'], {cwd: repoRoot}))
+)();
