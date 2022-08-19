39c15c147feb2ed337fcc5b49c0f00ad42cdfeed
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-08-18 22:20:12 -0400

    issue-#57 - Updated comment in 'src/object'.

--- packages/fjl/src/object/of.ts
+++ packages/fjl/src/object/of.ts
@@ -4,12 +4,11 @@ import {Constructable} from "../types";
 
 /**
  * Creates a value `of` given type;  Checks for one of the following construction strategies (in order listed):
- * @example
- * // - If exists `(value).constructor.of` uses this.
- * // - If value is of one String, Boolean, Symbol, or Number types calls it's
- * //      constructor as a function (in cast form;  E.g., `constructor(...args)` )
- * // - Else if constructor is a function, thus far, then calls constructor using
- * //      the `new` keyword (with any passed in args).
+ * - If exists `(value).constructor.of` uses this.
+ * - If value is a primitive [String, Boolean, Symbol, Number] calls it's
+ *   constructor as a function (in cast form;  E.g., `constructor(...args)` )
+ * - Else if constructor is a function, thus far, then calls constructor using
+ *   the `new` keyword (with any passed in args).
  */
 export const of = <T>(x: T, ...args: any[]): T => {
     if (!isset(x)) {
