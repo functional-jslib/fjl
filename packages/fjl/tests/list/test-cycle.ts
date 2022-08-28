8eff8d95908be1b856857311842e22e9c5800baf
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-08-28 11:22:10 -0400

    issue-#43,#61 - Updated 'cycle' tests. - All tests green.

--- packages/fjl/tests/list/test-cycle.ts
+++ packages/fjl/tests/list/test-cycle.ts
@@ -5,9 +5,9 @@ const {stringify} = JSON;
 describe('#cycle', () => {
   (<[Parameters<typeof cycle>, ReturnType<typeof cycle>][]>[
     [[5, 'x'], 'xxxxx'],
-    [[5, ['x']], 'xxxxx'.split('').map(c => [c])],
-    [[5, ''], Array(5).fill('', 0, 5)],
-    [[5, []], Array(5).fill([], 0, 5)],
+    [[5, ['x']], 'xxxxx'.split('')],
+    [[5, ''], ''],
+    [[5, []], []],
   ])
     .forEach(([args, expected]) => {
       it(`cycle(${args.map(x => stringify(x)).join(', ')}) === ` +
