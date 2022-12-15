7062e5aad946785842c1a756fd11d282616e872e
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-12-15 16:51:48 -0500

    issue-#57 - Removed 'Slice' type from '*cycle' method impls.

--- packages/fjl/src/list/cycle.ts
+++ packages/fjl/src/list/cycle.ts
@@ -1,7 +1,6 @@
 import {concat} from "./concat";
 import {replicate} from "./replicate";
 import {genIterator} from "./iterate";
-import {Slice} from "../types";
 import {append} from "./append";
 
 export const
@@ -9,15 +8,14 @@ export const
   /**
    * Replicates a list *n* number of times and appends the results (concat)
    */
-  cycle = <T>(n: number, xs: Slice<T>): Slice<T> =>
+  cycle = (n: number, xs): typeof xs =>
     concat(replicate(n, xs)),
 
-  $cycle = <T>(n: number) =>
-    (xs: Slice<T>): Slice<T> => cycle(n, xs),
+  $cycle = (n: number) => xs => cycle(n, xs),
 
   /**
    * Generates a generator which cycles list (concatenate) to end of last yielded result - On first call last yielded
    * result is initial list.
    */
-  genCycler = <T>(xs: Slice<T>): Generator<Slice<T>, void, Slice<T>> =>
+  genCycler = (xs): Generator<typeof xs, void, typeof xs> =>
     genIterator(_xs => append(_xs, xs), xs)();
