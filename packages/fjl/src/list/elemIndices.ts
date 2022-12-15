b48638f3826d2a76faaf3470542868c744d7f201
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2022-12-15 16:53:48 -0500

    issue-#57 - Removed 'Slice' type from '*elemIndices' method impls.

--- packages/fjl/src/list/elemIndices.ts
+++ packages/fjl/src/list/elemIndices.ts
@@ -1,12 +1,12 @@
 import {findIndices} from "./findIndices";
-import {Slice, Unary} from "../types";
+import {Unary} from "../types";
 import {$equal} from "../boolean/equal";
 
 /**
  * Returns found "value" indices.
  */
-export const elemIndices = <T>(value: T, xs: Slice<T>): number[] => findIndices($equal(value) as Unary, xs),
+export const elemIndices = (value, xs): number[] => findIndices($equal(value) as Unary, xs),
 
-  $elemIndices = <T>(value: T) =>
-    (xs: Slice<T>): number[] => elemIndices(value, xs)
+  $elemIndices = value =>
+    (xs): number[] => elemIndices(value, xs)
 ;
