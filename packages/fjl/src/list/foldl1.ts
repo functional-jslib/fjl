afacdf5fdebded6fa328a58bb23d080c80909906
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-01-14 23:16:45 -0500

    - fjl.fold1 - Removed 'Slice' type. - fjl.uncons - Updated docs.

--- packages/fjl/src/list/foldl1.ts
+++ packages/fjl/src/list/foldl1.ts
@@ -1,20 +1,20 @@
 import {uncons} from "./uncons";
 import {reduce} from "./utils/reduce";
-import {ReduceOp, Slice} from "../types";
+import {ReduceOp} from "../types";
 
 export const
 
   /**
    * A variant of `foldl` except without requiring the starting point value.  The starting point/value gets referenced internally (slice's first value) by the method.
    */
-  foldl1 = <T>(op: ReduceOp, xs: Slice<T>): ReturnType<typeof op> => {
-    const parts = uncons(xs);
+  foldl1 = <T>(op: ReduceOp, xs: T[]): ReturnType<typeof op> => {
+    const parts = uncons(xs) as [T, T[]];
     if (!parts) return;
     const [_head, _tail] = parts;
     return reduce(op, _head, _tail);
   },
 
   $foldl1 = <T>(op: ReduceOp) =>
-    (xs: Slice<T>): ReturnType<typeof op> => foldl1(op, xs)
+    (xs: T[]): ReturnType<typeof op> => foldl1(op, xs)
 
 ;
--- packages/fjl/src/list/uncons.ts
+++ packages/fjl/src/list/uncons.ts
@@ -1,5 +1,6 @@
 /**
- * Returns `head` and `tail` of passed in slice type (array/string like) in a tuple, or `undefined` if slice is empty.
+ * Returns `head` and `tail` of passed in slice type (array/string like) in a tuple,
+ * or `undefined` if incoming type is empty.
  */
 export const uncons = (xs: string | any[]): [string | any, typeof xs] | void =>
   !xs?.length ? undefined : [xs[0], xs.slice(1)];
