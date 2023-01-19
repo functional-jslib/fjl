0b3702738de98faa9056706ee6f4661bc8d86d81
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2023-01-18 20:53:17 -0500

    - fjl.assocList module - Formatted code, and performed small optimizations to methods in module. - fjl.fromAssocList - Changed method signature to accept an object to fold onto (instead of passing in an object constructor makes more sense to just accept the zero value, as we are now doing).

--- packages/fjl/src/object/assocList.ts
+++ packages/fjl/src/object/assocList.ts
@@ -1,43 +1,47 @@
 /**
  * @todo Should be using new methods here (Object.fromEntries, etc.) and deprecating existing ones (in favor of fjl.native.*, and or natively built-in ones).
  */
-import {isArray, isType} from './is';
+import {isArray} from './is';
 import {keys} from '../platform/object';
 import {TypeConstructor} from "../types";
+import {map} from "../list/map";
+import {foldl} from "../list/foldl";
 
 export const
 
-    /**
-     * Returns an associated list from given object.
-     */
-    toAssocList = <T>(obj: T): [keyof T, any][] => Object.entries(obj) as [keyof T, any][],
+  /**
+   * Returns an associated list from given object.
+   */
+  toAssocList = <T>(obj: T): [keyof T, any][] => Object.entries(obj) as [keyof T, any][],
 
-    /**
-     * Returns an associated list from given object (deeply (on incoming object's type)).
-     */
-    toAssocListDeep = (obj, TypeConstraint: TypeConstructor = Object) => keys(obj).map(key =>
-        TypeConstraint && isType(TypeConstraint, obj[key]) ?
-            [key, toAssocListDeep(obj[key], TypeConstraint)] :
-            [key, obj[key]]
+  /**
+   * Returns a deep associated list from given object, on incoming `TypeConstraint` constructor.
+   */
+  toAssocListDeep = (obj, TypeConstraint: TypeConstructor = Object) =>
+    map(key =>
+        TypeConstraint === obj[key]?.constructor ?
+          [key, toAssocListDeep(obj[key], TypeConstraint)] :
+          [key, obj[key]],
+      keys(obj)
     ),
 
-    /**
-     * From associated list to object.
-     */
-    fromAssocList = (xs, OutType = Object) => xs.reduce((agg, [key, value]) => {
-        agg[key] = value;
-        return agg;
-    }, new OutType()),
+  /**
+   * From associated list to zero object.
+   */
+  fromAssocList = (xs, zero = {}) => foldl((agg, [key, value]) => {
+    agg[key] = value;
+    return agg;
+  }, zero, xs),
 
-    /**
-     * From associated list to object (deep conversion on associative lists (array of 2 value arrays)).
-     */
-    fromAssocListDeep = (xs, OutType = Object) => xs.reduce((agg, [key, value]) => {
-        if (isArray(value) && isArray(value[0]) && value[0].length === 2) {
-            agg[key] = fromAssocListDeep(value, OutType);
-            return agg;
-        }
-        agg[key] = value;
-        return agg;
-    }, new OutType())
+  /**
+   * From associated list to object (deep conversion on associative lists (array of 2 value arrays)).
+   */
+  fromAssocListDeep = (xs, OutType = Object) => foldl((agg, [key, value]) => {
+    if (isArray(value) && isArray(value[0]) && value[0].length === 2) {
+      agg[key] = fromAssocListDeep(value, OutType);
+      return agg;
+    }
+    agg[key] = value;
+    return agg;
+  }, new OutType(), xs)
 ;
