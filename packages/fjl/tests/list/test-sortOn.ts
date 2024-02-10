2ee8c1575ca1f5d4b3d1eb605b2bf9db482f685d
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2024-02-09 23:50:25 -0500

    issue-#57 - Removed use of 'curry_' module, in tests, and removed unused methods in tests/helpers..

--- packages/fjl/tests/list/test-sortOn.ts
+++ packages/fjl/tests/list/test-sortOn.ts
@@ -1,5 +1,5 @@
 import {range, reverse, $sortOn, $take} from "../../src/list";
-import {__, alphabetArray, expectEqual} from "../helpers";
+import {alphabetArray, expectEqual} from "../helpers";
 import {compose} from "../../src/function";
 import {Unary} from "../../src/types";
 
@@ -11,13 +11,13 @@ describe('#sortOn', () => {
   it('should sort a list in ascending order', () => {
     expectEqual(sortOnIdentity(range10To0), range0To10);
     expectEqual(sortOnIdentity(range0To10), range0To10);
-    compose(expectEqual(__, alphabetArray), sortOnIdentity, reverse)(alphabetArray);
+    compose((value) => expectEqual(value, alphabetArray), sortOnIdentity, reverse)(alphabetArray);
     compose(/*log,*/ sortOnIdentity, reverse)(alphabetArray);
   });
   it('should return a copy of original list when said list is already sorted', () => {
-    compose(expectEqual(__, ['a', 'b', 'c']), sortOnIdentity, $take(3) as unknown as Unary<string[]>)(alphabetArray);
-    compose(expectEqual(__, alphabetArray), sortOnIdentity)(alphabetArray);
-    compose(expectEqual(__, range0To10), sortOnIdentity)(range0To10);
+    compose((value) => expectEqual(value, ['a', 'b', 'c']), sortOnIdentity, $take(3) as unknown as Unary<string[]>)(alphabetArray);
+    compose((value) => expectEqual(value, alphabetArray), sortOnIdentity)(alphabetArray);
+    compose((value) => expectEqual(value, range0To10), sortOnIdentity)(range0To10);
   });
   it('should return an empty list when receiving an empty list', () => {
     expectEqual(sortOnIdentity([]), []);
