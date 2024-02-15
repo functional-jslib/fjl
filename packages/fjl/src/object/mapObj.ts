517e47e4bec5917fa91bdd377330852bc2e20c9c
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2024-02-15 11:58:01 -0500

    issue-#57 - Patch on 'mapObj' impl. to ensure passed in object type constructors are used as 'map' base.  'mapObj', and 'searchObj', Docblock updates.

--- packages/fjl/src/object/mapObj.ts
+++ packages/fjl/src/object/mapObj.ts
@@ -1,14 +1,16 @@
 import {reduce} from "../list/utils";
+import {MapOp} from "../types";
+import {of} from "./of";
 
 /**
- * @deprecated Map over/reduce objects entries, values, and/or, keys,
+ * @deprecated Map over/reduce object's entries, values, and/or, keys,
  * (directly) as required.
  *
  * Maps a function over an objects own key, and values.
  */
-export const mapObj = (fn, obj) =>
+export const mapObj = <T extends object>(fn: MapOp, obj: T): T =>
   reduce((agg, [key, value]) => {
     agg[key] = fn(value, key, obj);
     return agg;
-  }, new obj.constructor(), obj.entries())
+  }, of(obj), Object.entries(obj))
 ;
