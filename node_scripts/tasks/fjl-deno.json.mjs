ad4076b5007537ad1fd9ffbfb8323ee2b89fbae6
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2024-08-07 00:09:47 -0400

    feat: #55-deno-support - Notes
    - Scaffolding for 'deno.json.imports'
      field script.

---
+++ node_scripts/tasks/fjl-deno.json.mjs
@@ -0,0 +1,13 @@
+import fs from 'node:fs/promises';
+import path from "node:path";
+
+const fjlRoot = path.join(__dirname, '../../');
+
+await (async () => {
+
+  // Read 'fjl/src'
+  // Scaffold out the matching 'deno.json.imports' field
+  // for all source imports to
+  // work as they are (without file extensions).
+
+})();
