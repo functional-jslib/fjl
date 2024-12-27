2dcc022ef5d2edb828443cf9d5376741d8c1835a
Ely De La Cruz <603428+elycruz@users.noreply.github.com>
2024-12-26 20:20:25 -0500

    feat: issue-#57 - type fix
    - Fix empty type in
      'setTheory' module.

--- packages/fjl/src/object/setTheory.ts
+++ packages/fjl/src/object/setTheory.ts
@@ -11,7 +11,7 @@ export const
   objIntersect = <T extends object, T2 extends object>(
     obj1: T,
     obj2: T2
-  ): { [index in keyof T] } =>
+  ): { [index in keyof T]: any } =>
     reduce((agg, key) => {
       if (hasOwnProperty(obj2, key)) {
         agg[key] = obj2[key];
@@ -20,24 +20,24 @@ export const
     }, {}, keys(obj1)),
 
   $objIntersect = <T extends object, T2 extends object>(obj1: T) =>
-    (obj2: T2): { [index in keyof T] } => objIntersect(obj1, obj2),
+    (obj2: T2): { [index in keyof T]: any } => objIntersect(obj1, obj2),
 
-  objDifference = <T extends object, T2 extends object>(obj1: T, obj2: T2): { [index in keyof T] } =>
+  objDifference = <T extends object, T2 extends object>(obj1: T, obj2: T2): { [index in keyof T]: any } =>
     reduce((agg, key: string) => {
       if (!hasOwnProperty(obj2, key)) {
         agg[key] = obj1[key];
       }
       return agg;
-    }, {} as { [index in keyof T] }, keys(obj1)),
+    }, {} as { [index in keyof T]: any }, keys(obj1)),
 
   $objDifference = <T extends object, T2 extends object>(obj1: T) =>
-    (obj2: T2): { [index in keyof T] } => objDifference(obj1, obj2),
+    (obj2: T2): { [index in keyof T]: any } => objDifference(obj1, obj2),
 
-  objComplement = <T extends object, T2 extends object>(obj0: T, ...objs: T2[]): { [index in keyof T] } =>
+  objComplement = <T extends object, T2 extends object>(obj0: T, ...objs: T2[]): { [index in keyof T]: any } =>
     reduce((agg, obj) =>
-      assignDeep(agg, objDifference(obj, obj0)), {} as { [index in keyof T] }, objs),
+      assignDeep(agg, objDifference(obj, obj0)), {} as { [index in keyof T]: any }, objs),
 
   $objComplement = <T extends object, T2 extends object>(obj0: T) =>
-    (...objs: T2[]): { [index in keyof T] } => objComplement(obj0, ...objs)
+    (...objs: T2[]): { [index in keyof T]: any } => objComplement(obj0, ...objs)
 
 ;
